3b337ff42d082ad0704b4581947a9b6346d190b1
George Krommydas <geokrom98@gmail.com>
2022-05-20 13:06:45 +0300

    Add files via upload

--- Exercise-2/Ex1/cuinfo.cu
+++ Exercise-2/Ex1/cuinfo.cu
@@ -1,3 +1,6 @@
+/* Name: Georgios Krommydas
+ * A.M.: 3260
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
@@ -17,10 +20,21 @@ void cuinfo_print_devinfo()
 		printf("No CUDA devices found.\n");
 		return;
 	}
-	
+
 	for (i = 0; i < num_devs; i++)
 	{
+		cudaGetDeviceProperties(&dev_prop, i);
 		/* TODO: Retrieve and pretty-print all the necessary information */
+		printf("----------------------------------------------------------\n");	
+		printf("Information for CUDA Device - %d\n", i+1);
+		printf("----------------------------------------------------------\n");	
+		printf("Device Name: %s\n", dev_prop.name);
+		printf("Device CUDA compute capability: %d.%d\n", dev_prop.major, dev_prop.minor);
+		printf("Device number of streaming multiprocessors: %d\n", dev_prop.multiProcessorCount);
+		printf("Device max number of threads per block: %d\n", dev_prop.maxThreadsPerBlock);
+		printf("Device size of global memory: %u bytes\n", dev_prop.totalGlobalMem);
+		printf("Device size of shared memory per block: %u bytes\n", dev_prop.sharedMemPerBlock);
+		printf("----------------------------------------------------------\n");
 	}
 }
 
